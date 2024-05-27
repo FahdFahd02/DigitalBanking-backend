9f6c222330f4f1a2e78eb8d77b991919cda02512
Fahd <oulaizmedfahdpro@gmail.com>
2024-05-28 00:14:43 +0100

    backend with security

--- src/main/java/org/sid/oulaizebankingbackend/web/BankAccountRestAPI.java
+++ src/main/java/org/sid/oulaizebankingbackend/web/BankAccountRestAPI.java
@@ -1,18 +1,15 @@
 package org.sid.oulaizebankingbackend.web;
 
-import org.sid.oulaizebankingbackend.dtos.AccountHistoryDTO;
-import org.sid.oulaizebankingbackend.dtos.AccountOperationDTO;
-import org.sid.oulaizebankingbackend.dtos.BankAccountDTO;
+import org.sid.oulaizebankingbackend.dtos.*;
+import org.sid.oulaizebankingbackend.exceptions.BalanceNotSufficientException;
 import org.sid.oulaizebankingbackend.exceptions.BankAccountNotFoundException;
 import org.sid.oulaizebankingbackend.services.BankAccountService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 public class BankAccountRestAPI {
     private BankAccountService bankAccountService;
     public BankAccountRestAPI(BankAccountService bankAccountService) {
@@ -36,5 +33,22 @@ public class BankAccountRestAPI {
             @RequestParam(name="page",defaultValue = "0") int page,
             @RequestParam(name="size",defaultValue = "5")int size) throws BankAccountNotFoundException {
         return bankAccountService.getAccountHistory(accountId,page,size);
+    }
+    @PostMapping("/accounts/debit")
+    public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
+        return debitDTO;
+    }
+    @PostMapping("/accounts/credit")
+    public CreditDTO credit(@RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException {
+        this.bankAccountService.credit(creditDTO.getAccountId(),creditDTO.getAmount(),creditDTO.getDescription());
+        return creditDTO;
+    }
+    @PostMapping("/accounts/transfer")
+    public void transfer(@RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.transfer(
+                transferRequestDTO.getAccountSource(),
+                transferRequestDTO.getAccountDestination(),
+                transferRequestDTO.getAmount());
     }
 }
